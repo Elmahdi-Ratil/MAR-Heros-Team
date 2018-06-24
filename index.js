09f2c400c5009b74c4c57ddb08a47ddcb48341a1
heros4 <40445911+heros4@users.noreply.github.com>
2018-06-24 02:53:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -563,30 +563,7 @@ const codes = {
   };
   });
 
- client.on("message", message => {
-    var prefix = "~";
- 
-            var args = message.content.substring(prefix.length).split(" ");
-            if (message.content.startsWith(prefix + "clear")) {
-   if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('⚠ | **لا يوجد لديك صلاحية لمسح الشات**');
-        var msg;
-        msg = parseInt();
-      
-      message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
-      message.channel.sendMessage("", {embed: {
-        title: "Done | تــم مسح الشات",
-        color: 0x06DF00,
-        description: "تم مسح الرسائل ",
-        footer: {
-          text: "©zabhm"
-        }
-      }}).then(msg => {msg.delete(3000)});
-                          }
-
-     
-}); 
-
-const superagent = require('superagent');
+ const superagent = require('superagent');
 
 client.on('message' , async (message) => {
        if(message.content.startsWith(prefix + "meme")) {
@@ -614,10 +591,6 @@ client.on('message' , async (message) => {
 
             }
 }); 
-
-
-
-
 
 
 
