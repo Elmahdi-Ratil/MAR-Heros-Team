d6e1bafa3d835945b15d756bc9af63a9fdea93d6
heros4 <40445911+heros4@users.noreply.github.com>
2018-06-24 02:40:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -503,22 +503,25 @@ message.channel.send(`${user} has ${inviteCount} invites.`);
   }
 });
 
-client.on('message' , async (message) => {
- if (message.content.startsWith(prefix + 'yn')) {
+client.on('message', message => {
+if (message.content.startsWith(prefix + 'infou')) {
+var year = message.createdAt.getFullYear()
+var month = message.createdAt.getMonth()
+var day = message.createdAt.getDate()
+     let embed = new Discord.RichEmbed()
+     .setAuthor(message.author.username, message.author.avatarURL)
+  .setThumbnail(message.author.avatarURL)
+    .addField("**اسمك:**",  '**[ ' + `${message.author.username}` + ' ]**')
+      .setThumbnail(message.author.avatarURL)
+               .setFooter(`${message.author.username}`, 'https://images-ext-2.discordapp.net/external/JpyzxW2wMRG2874gSTdNTpC_q9AHl8x8V4SMmtRtlVk/https/orcid.org/sites/default/files/files/ID_symbol_B-W_128x128.gif')
+  .addField('الكود الخاص بك:', message.author.discriminator)
+  .addField("**عدد الايام منذ افتتاح حسابك:**", message.author.createdAt.getDate())
+    .addField("** تم افتتاح حسابك عام:**", message.createdAt.getFullYear())
+        .addField("** عدد الشهور منذ افتتاح حسابك:**", message.createdAt.getMonth())
 
-let color = '0xffffff'
-
-      const { body } = await superagent
-    .get('https://yesno.wtf/api/');
-    if(body.answer === 'yes') color = '0x01DF01';
-    if(body.answer === 'no') color = '0xFF0000';
-    const embed = new Discord.RichEmbed()
-    .setColor(color)
-    .setImage(`${body.image}`)
-    message.channel.send(`**The magic API says:** **${body.answer}**`, {embed});
-
-}
-});
+  message.channel.send({embed});
+    }
+})
 
 
 
