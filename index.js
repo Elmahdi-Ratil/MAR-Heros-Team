d31bbacadf52fea976235d8386d7c27cdd497be3
heros4 <40445911+heros4@users.noreply.github.com>
2018-06-24 02:37:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -492,7 +492,16 @@ __امامك  5 توان للاختيار__`)
    }
 });
 
-
+client.on('message', message => {
+   if(message.content.startsWith(prefix + "invites")) {
+    message.guild.fetchInvites().then(invs => {
+      let user = message.mentions.users.first() || message.author
+      let personalInvites = invs.filter(i => i.inviter.id === user.id);
+      let inviteCount = personalInvites.reduce((p, v) => v.uses + p, 0);
+message.channel.send(`${user} has ${inviteCount} invites.`);
+});
+  }
+});
 
 
 
