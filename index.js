1358eb681164c0f46108a7c398cf4de879c2229c
heros4 <40445911+heros4@users.noreply.github.com>
2018-06-27 20:15:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,33 +2,12 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '!'
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`Mido King |`,"https://www.twitch.tv/lboghaz_kingheros")
-  console.log('')
-  console.log('')
-  console.log('+[-----------------------------------------------------------------]+')
-  console.log(`[Start] ${new Date()}`);
-  console.log('+[-----------------------------------------------------------------]+')
-  console.log('')
-  console.log('+[------------------------------------]+');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('By The Atlantic Enjoy')
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('+[------------------------------------]+')
-  console.log('')
-  console.log('+[------------]+')
-  console.log(' Bot Is Online')
-  console.log('+[------------]+')
-  console.log('')
-  console.log('')
+
+client.on('ready', function() {
+	console.log(`i am ready ${client.user.username}`);
+    client.user.setGame(prefix + 'Mido King ||');
 });
+
 
 client.on('message',function(message) {
     let messageArray = message.content.split(" ");
