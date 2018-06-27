322175a9919a79051e2aa82cf3214a6fd7ed582b
heros4 <40445911+heros4@users.noreply.github.com>
2018-06-27 21:53:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,32 @@ client.on('ready', function() {
     client.user.setGame(prefix + 'Mido King ||');
 });
 
+client.on('message', message => {
+   if (message.content.startsWith("idd")) {
+                if(!message.channel.guild) return message.reply('** This command only for servers**');
+
+               var mentionned = message.mentions.users.first();
+    var mentionavatar;
+      if(mentionned){
+          var mentionavatar = mentionned;
+      } else {
+          var mentionavatar = message.author;
+          
+      }
+   let embed = new Discord.RichEmbed()
+  .setColor("RANDOM")
+   .setThumbnail(`${mentionavatar.avatarURL}`)
+  .addField("Name:",`<@` + `${mentionavatar.id}` + `>`, true)
+  .addField('Discrim:',"#" +  `${mentionavatar.discriminator}`, true)
+   .addField("ID:", "**[" + `${mentionavatar.id}` + "]**", true)
+  .addField("Create At:", "**[" + `${mentionavatar.createdAt}` + "]**", true)
+     
+     
+  message.channel.sendEmbed(embed);
+  console.log('[id] Send By: ' + message.author.username)
+    }
+});
+
 
 client.on('message',function(message) {
     let messageArray = message.content.split(" ");
