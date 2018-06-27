f1e46dffab38c2efde1509da2a4db99d043bea78
heros4 <40445911+heros4@users.noreply.github.com>
2018-06-27 05:21:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -740,25 +740,6 @@ member.guild.channels.find('id', welc[member.guild.id].imgwelc).sendFile(canvas.
 
 
 
-})
-})
-
-});
-client.on('message', message => {
-  if (message.author.bot) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-
-  let args = message.content.split(" ").slice(1);
-
-  if (command == "~say") {
-   message.channel.sendMessage(args.join("  "))
-   message.delete()
-  }
- });
-
 
 
 
