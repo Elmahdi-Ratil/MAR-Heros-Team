2fb4cb62c248e5f54e4df39ed831e294bf04d07c
heros4 <40445911+heros4@users.noreply.github.com>
2018-06-28 19:46:08 +0100

    Update index.js

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ client.on('voiceStateUpdate', (o, n) => {
     let oldUserChannel = o.voiceChannel
 
     var channel = client.channels.get("461643051459149834");
-        let cha = n.guild.channels.get("461642960073654283");
+        let cha = o.guild.channels.get("461642960073654283");
         
           let mute1 = o.serverMute;
   let mute2 = n.serverMute;
