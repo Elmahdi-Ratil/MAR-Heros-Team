d9c3c649a0e7a1f2b44fd865245f7a277414a468
heros4 <40445911+heros4@users.noreply.github.com>
2018-06-28 20:06:36 +0100

    Update index.js

--- index.js
+++ index.js
@@ -29,29 +29,6 @@ client.on('message', message => {
     }
 });
 
-client.on('voiceStateUpdate', (o, n) => {
-        let newUserChannel = n.voiceChannel
-    let oldUserChannel = o.voiceChannel
-
-    var channel = client.channels.get("461642960073654283");
-        let cha = n.guild.channels.get("461643051459149834");
-
-          let mute1 = o.serverMute;
-  let mute2 = n.serverMute;
-
-
-  let deafen1 = o.serverDeaf;
-  let deafen2 = n.serverDeaf;
-
-    if(mute1 === false && mute2 === true) return;
-    if(mute1 === true && mute2 === false) return;
-    if(deafen1 === false && deafen2 === true) return;
-    if(deafen1 === true && deafen2 === false) return;
-
-
-    channel.send(Join Room ${n.displayName})
-
-})
 
 
 
