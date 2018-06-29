7043c7f6c064d4ffcb553b3c0b147bdd748d8370
heros4 <40445911+heros4@users.noreply.github.com>
2018-06-30 00:40:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -585,24 +585,6 @@ client.on('message', message => {
 
     }
   });
-client.on("message", message => {             
-  const prefix = '+'
-          if(!message.channel.guild) return;
-   if(message.author.bot) return;
-      if(message.content === prefix + "serverimage"){ 
-          const embed = new Discord.RichEmbed()
-  
-      .setTitle(`This is  ** ${message.guild.name} **  Photo !`)
-  .setAuthor(message.author.username, message.guild.iconrURL)
-    .setColor(0x164fe3)
-    .setImage(message.guild.iconURL)
-    .setURL(message.guild.iconrURL)
-                    .setTimestamp()
-
-   message.channel.send({embed});
-      }
-  });
-
 
 
 
