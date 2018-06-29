00b57676ec5df12e4b644f195635edd306ed547f
heros4 <40445911+heros4@users.noreply.github.com>
2018-06-30 00:35:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -654,22 +654,6 @@ m.removeRole(message.guild.roles.find('name', MRole))
 message.reply('*** Done ✅  ***').then(msg => {msg.delete(10000)});
 }
 });
-client.on("message",  message => {
-    var prefix = "+";
-    let args = message.content.split(' ').slice(1);
-if(message.content.startsWith(prefix + 'nickname')) {
-   if (!message.member.hasPermission("MANAGE_NICKNAMES")) {
-       message.channel.send("ضع الاسم")
-   } else {
-       if (!message.guild.member(client.user).hasPermission('MANAGE_NICKNAMES')) return message.reply(' ❌البوت ما عنده خاصية MANAGE_NICKNAMES.').catch(console.error);
-       let changenick = message.mentions.users.first();
-       let username = args.slice(1).join(' ')
-       if (username.length < 1) return message.reply('ضع الاسم').catch(console.error);
-       if (message.mentions.users.size < 1) return message.author.send('You must mention a user to change their nickname. ❌').catch(console.error);
-       message.guild.member(changenick.id).setNickname(username);
-       message.channel.send("تم تغيير الاسم الى: " + changenick + "")
-   }
-}});
 
 
 
