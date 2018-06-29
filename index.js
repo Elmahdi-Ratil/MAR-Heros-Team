a007764f61b94e4ef379e51701a15093b57d7a6a
heros4 <40445911+heros4@users.noreply.github.com>
2018-06-30 00:42:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -571,20 +571,6 @@ if (message.content.startsWith(prefix + "uptime")) {
 
 }
 });
-client.on('message', message => {
-    if (message.author.bot) return;
-    if(message.content == '+member') {
-    const embed = new Discord.RichEmbed()
-    .addField(`حالة الأعضاء🔋`,'-',   true)
-.addField(`💚 اونلاين:   ${message.guild.members.filter(m=>m.presence.status == 'online').size}`,'-',   true)
-.addField(`❤ مشغول:     ${message.guild.members.filter(m=>m.presence.status == 'dnd').size}`,'-',   true)
-.addField(`💛 خامل:      ${message.guild.members.filter(m=>m.presence.status == 'idle').size}`,'-',   true)   
-.addField(`🖤 اوفلاين:   ${message.guild.members.filter(m=>m.presence.status == 'offline').size}`,'-',  true) 
-.addField(`💙   الكل:  ${message.guild.memberCount}`,'-',   true)         
-         message.channel.send({embed});
-
-    }
-  });
 
 
 
