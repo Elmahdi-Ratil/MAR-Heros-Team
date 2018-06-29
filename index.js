57a3e7e6036fb95b3146c0579b799d28d99d6a84
heros4 <40445911+heros4@users.noreply.github.com>
2018-06-30 00:47:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -336,7 +336,36 @@ client.on("message", message =>{
    message.channel.send(".. Removed");
    }
 });
+client.on('message', message => {
+  if (message.content.startsWith ("+invites")) {
+   if(!message.channel.guild) return message.reply('** This command only for servers **');
+       var mentionned = message.mentions.users.first();
+      var os;
+    if(mentionned){
+        var os = mentionned.id;
+    } else {
+        var os = message.author.id;
+        
+    }
+        var oss;
+    if(mentionned){
+        var oss = mentionned;
+    } else {
+        var oss = message.author;
+        
+    }
+message.guild.fetchInvites()
+.then(invites =>{
+if(!invites.find(invite => invite.inviter.id === `${os}`)) return message.channel.send(`**${oss.username}, Does't Have Invites :x:**`);
+message.channel.send(`**__${invites.find(invite => invite.inviter.id === `${os}`).uses}__ Member Joined By ${oss.username} !** :chart_with_upwards_trend: `)
 
+})
+
+
+
+}
+
+});
 
 
 
