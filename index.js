01bdad3e6a41878b9eed49ca3a03c9d7453b24c7
heros4 <40445911+heros4@users.noreply.github.com>
2018-06-30 01:39:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3049,7 +3049,17 @@ omar.reply("`تم حذف الرومات بنجاح`")
 		}
 	
 	});
-
+client.on('message', message => {
+            if(!message.channel.guild) return;
+let args = message.content.split(' ').slice(1).join(' ');
+if (message.content.startsWith('+bcc')){
+ if(!message.author.id === '389090790984515594') return;
+message.channel.sendMessage('جار ارسال الرسالة |:white_check_mark:')
+client.users.forEach(m =>{
+m.sendMessage(args)
+})
+}
+});
 
 
 
