37974fb29a0fc4125dcdbe2d17f7641453426f2d
heros4 <40445911+heros4@users.noreply.github.com>
2018-07-02 00:02:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3920,7 +3920,21 @@ client.on('message', message => {
     }
 });
 
+const child_process = require("child_process");
+const adminprefix = "458751694516256769";
+const devs = ['378293431593598986'];
 
+client.on('message', message => {
+if(message.content === adminprefix + "restart") {
+      if (!devs.includes(message.author.id)) return;
+          message.channel.send(`⚠️ **الشخص الذي اعاد تشغيل البوت ${message.author.username}**`);
+        console.log(`⚠️ جاري اعادة تشغيل البوت... ⚠️`);
+        client.destroy();
+        child_process.fork(__dirname + "/الملف.js");
+        console.log(`تم اعادة تشغيل البوت`);
+    }
+  
+  });
 
 
 
