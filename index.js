2c83941f210c93f7fdec25e31da42f5a1630f012
heros4 <40445911+heros4@users.noreply.github.com>
2018-07-05 17:09:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -190,6 +190,34 @@ if(message.content.startsWith(prefix + "voice")) {
 }
 });
 
+
+client.on("guildMemberAdd", function(member) {
+    const wc = member.guild.channels.find("name", "member-log")
+        const embed = new Discord.RichEmbed()
+        .setColor('B90C0C')
+        .setAuthor(member.user.tag, member.user.avatarURL)
+ .setDescription('***يا مرحبا وسهلاً بضيف لفانا، يزهي بك الأدب العربي وينثر لك أزهار يسقيك من نبع المشاعر وفانا، لين الهلا تثمر على غصونك أطيار. ***')
+.setThumbnail(member.avatarURL)
+  .setImage('https://cdn.pg.sa/OD53Wjr9p0.gif')
+        .setTimestamp()
+        return wc.sendEmbed(embed);
+        
+});
+client.on('guildMemberRemove', member => {
+    var embed = new Discord.RichEmbed()
+    .setAuthor(member.user.username, member.user.avatarURL)
+    .setThumbnail(member.user.avatarURL)
+    .setTitle(`خرج عضو`)
+    .setDescription(`الى اللقاء...`)
+    .addField(':bust_in_silhouette:   تبقي',`**[ ${member.guild.memberCount} ]**`,true)
+    .setColor('RED')
+    .setFooter(`The King Bot`, '')
+
+var channel =member.guild.channels.find('name', 'wlc')
+if (!channel) return;
+channel.send({embed : embed});
+});
+
 client.on('message', message => {
     if (message.content.startsWith("رابط")) {
 
