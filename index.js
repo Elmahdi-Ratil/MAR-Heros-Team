3bec4a522ed645e78e06b56e4f87cdca91fbae23
heros4 <40445911+heros4@users.noreply.github.com>
2018-07-16 05:09:03 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3855,10 +3855,27 @@ client.on('message', message => {
 
 
 
-  
-  
-  
-
+        client.on('message', function(message) {
+    if (!message.member.hasPermissions(['ADMINISTRATOR'])){
+            let command = message.content.split(" ")[0];
+        if(message.content.includes('discord.gg')){// gg شيل المسافه الي بين النقطق وال
+        if(!message.channel.guild) return;
+        message.delete()
+           if(!message.channel.guild) return message.reply('** This command only for servers**');
+     message.member.addRole(message.guild.roles.find('name', 'Muted'));
+    const embed500 = new Discord.RichEmbed()
+      .setTitle(" - Alert")
+            .addField(`**  You Have Been Muted **` , `**Reason : Sharing Another Discord Link**`)
+            .setColor("c91616")
+            .setThumbnail(`${message.author.avatarURL}`)
+            .setAuthor(message.author.username, message.author.avatarURL)
+        .setFooter(`${message.guild.name} `)
+     message.channel.send(embed500)
+   
+       
+    }
+    }
+})
 
 
 
