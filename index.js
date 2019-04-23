a6e850b735005295fc29a0325c146fbdc2dff18f
mahdi <40445911+heros4@users.noreply.github.com>
2019-04-23 20:07:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3827,6 +3827,34 @@ message.channel.sendEmbed(embed).then();
 
 
 
+client.on('message', async message => {
+  if(message.content.startsWith(prefix + "voicesetup")) {
+  if(!message.guild.member(message.author).hasPermissions('MANAGE_CHANNELS')) return message.reply(':x: ليس لديك الصلاحيات الكافية');
+  if(!message.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS','MANAGE_ROLES_OR_PERMISSIONS'])) return message.reply(':x: ليس معي الصلاحيات الكافية');
+  var args = message.content.split(' ').slice(1).join(' ');
+  if(args && !args.includes(0)) return message.channel.send(':negative_squared_cross_mark: » فشل اعداد الروم الصوتي .. يجب عليك كتابة 0 في اسم الروم');
+  if(!args) args = VoiceOnline: [ ${message.guild.members.filter(s => s.voiceChannel).size} ];
+  message.channel.send(':white_check_mark: » تم عمل الروم الصوتي بنجاح');
+  message.guild.createChannel(${args.replace(0, message.guild.members.filter(s => s.voiceChannel).size)}, 'voice').then(c => {
+    c.overwritePermissions(message.guild.id, {
+      CONNECT: false,
+      SPEAK: false
+    });
+    setInterval(() => {
+      c.setName(${args.replace(0, message.guild.members.filter(s => s.voiceChannel).size)}).catch(err => {
+        if(err) return;
+      });
+    },3000);
+  });
+  }
+});
+
+
+
+
+
+
+
 
 
 
