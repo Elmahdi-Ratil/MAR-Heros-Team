daba128b6ca57081c2d5753a776e13fead752f40
mahdi <40445911+heros4@users.noreply.github.com>
2019-07-04 18:41:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,21 +2,7 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const client = new Discord.Client();
 const prefix = '+'
-client.on('ready', () => {
-  console.log('╔[════════════════════════════════════]╗');
-  console.log('')
-  console.log('            ╔[════════════]╗')
-  console.log('              Bot Is Online')
-  console.log('            ╚[════════════]╝')
-  console.log('')
-  console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log('')
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log('')
-  console.log('╚[════════════════════════════════════]╝')
-});
+
 client.on('ready', () => {
      client.user.setActivity("+help",{type: 'playing M.A.R'})
 
